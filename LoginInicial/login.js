6ce043a23ea5f09a4c061feb9f3cd3c6f2cc1add
Jordan Moreira <jordanmoreira231@gmail.com>
2023-08-11 01:38:56 -0300

    otimizacoes

--- LoginInicial/login.js
+++ LoginInicial/login.js
@@ -3,10 +3,10 @@ function check() { //Para verificar se o nome e senha estão corretos
         senha = document.getElementById("Senha").value;
     console.log(nome);
     if (nome == "maria" && senha == "1234") {
-        location.href = ('http://127.0.0.1:5501/Saque/saque.html'); //abre a página de saque
+        location.href = ('http://127.0.0.1:5500/Saque/saque.html'); //abre a página de saque
     } else {
         if (nome == "joao" && senha == "4321") {
-            location.href = ('http://127.0.0.1:5501/Saque/saque.html');
+            location.href = ('http://127.0.0.1:5500/Saque/saque.html');
         } else {
             alert("Usuário ou senha iválidos"); //mensagem de erro
         }
--- Saque/saque.js
+++ Saque/saque.js
@@ -1,101 +1,70 @@
-
-
 const objCedulas = {
-    u: {
-        id: "c1",
-        result: "",
-        valor: 1
+    duzentos: {
+      id: "c200",
+      result: "",
+      valor: 200,
     },
-    doi: {
-        id: "c2",
-        result: "",
-        valor: 2
+    cem: {
+      id: "c100",
+      result: "",
+      valor: 100,
     },
-    cin: {
-        id: "c5",
-        result: "",
-        valor: 5
+    cinquenta: {
+      id: "c50",
+      result: "",
+      valor: 50,
     },
-    d: {
-        id: "c10",
-        result: "",
-        valor: 10
+    vinte: {
+      id: "c20",
+      result: "",
+      valor: 20,
     },
-    vi: {
-        id: "c20",
-        result: "",
-        valor: 20
+    dez: {
+      id: "c10",
+      result: "",
+      valor: 10,
     },
-    ci: {
-        id: "c50",
-        result: "",
-        valor: 50
-    },
-    c: {
-        id: "c100",
-        result: "",
-        valor: 100
-    },
-    du: {
-        id: "c200",
-        result: "",
-        valor: 200
-    }
-},
-    keysCedulas = Object.keys(objCedulas)
 
+    cinco: {
+      id: "c5",
+      result: "",
+      valor: 5,
+    },
+    dois: {
+      id: "c2",
+      result: "",
+      valor: 2,
+    },
+    um: {
+      id: "c1",
+      result: "",
+      valor: 1,
+    },
+  },
+  keysCedulas = Object.keys(objCedulas);
 
 function sacar() {
-    const valor = document.querySelector("#valor").value
-    notas(valor);
-    console.log(valor)
+  const valor = document.querySelector("#valor").value;
+  calculaNotas(valor);
+  mostraResultado();
 }
 
+function calculaNotas(valorSaque) {
+  keysCedulas.forEach((cedula) => {
+    let valorCedula = objCedulas[cedula].valor,
+      quantidadeCedulas =
+        (valorSaque - (valorSaque % valorCedula)) / valorCedula;
 
-function notas(valor) {
-    console.log(keysCedulas)
-
-    keysCedulas.forEach(cedula => {
-
-    })
-
-    let duz = (valor - (valor % 200)) / 200;
-    objCedulas.du.result = !!duz ? `${duz} nota(s) de R$200.` : "";
-    valor %= 200;
-
-    let cem = (valor - (valor % 100)) / 100;
-    objCedulas.c.result = !!cem ? `${cem} nota(s) de R$100.` : "";
-    valor %= 100;
-
-    let cinq = (valor - (valor % 50)) / 50;
-    objCedulas.ci.result = !!cinq ? `${cinq} nota(s) de R$50.` : "";
-    valor %= 50;
-
-    let vint = (valor - (valor % 20)) / 20;
-    objCedulas.vi.result = !!vint ? `${vint} nota(s) de R$20.` : "";
-    valor %= 20;
-
-    let dez = ((valor % 20) - (valor % 10)) / 10;
-    objCedulas.d.result = !!dez ? `${dez} nota(s) de R$10.` : "";
-    valor %= 10;
-
-    let cinc = (valor - (valor % 5)) / 5;
-    objCedulas.cin.result = !!cinc ? `${cinc} nota(s) de R$5.` : "";
-    valor %= 5;
-
-    let dois = (valor - (valor % 2)) / 2;
-    objCedulas.doi.result = !!dois ? `${dois} nota(s) de R$2.` : "";
-    valor %= 2;
-
-    let um = valor;
-    objCedulas.u.result = !!um ? `${um} nota(s) de R$1.` : "";
-
-    keysCedulas.forEach(cedula => {
-
-        document.querySelector(`#${objCedulas[cedula].id}`).value = objCedulas[cedula].result
-
-    });
+    objCedulas[cedula].result = !!quantidadeCedulas
+      ? `${quantidadeCedulas} nota(s) de R$${objCedulas[cedula].valor}.`
+      : "";
+    valorSaque %= valorCedula;
+  });
 }
 
-
-
+function mostraResultado() {
+  keysCedulas.forEach((cedula) => {
+    document.querySelector(`#${objCedulas[cedula].id}`).value =
+      objCedulas[cedula].result;
+  });
+}
